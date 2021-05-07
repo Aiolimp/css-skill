94d72ede698d2a08200c4289eafa81e0d401a15c
Aiolimp <172978010@qq.com>
2021-05-07 17:46:48 +0800

    :rocket: 添加CSS样式

--- src/app.vue
+++ src/app.vue
@@ -1,25 +1,40 @@
 <template>
-	<div class="index-page flex-ct-y">
-		<h1>
-			<span class="gradient">bruce-cli</span>
-			<img src="./assets/img/logo.svg">
-		</h1>
-		<p>How to configure this <strong class="vue">Vue</strong> project</p>
-		<p>Check out the `<a href="https://github.com/JowayYoung/bruce-cli" class="gradient">bruce-cli docs</a>`</p>
-	</div>
+  <div class="index-page">
+    <div class="header flex-ct-y">
+      <h1>
+        <span class="gradient">bruce-cli</span>
+        <img src="./assets/img/logo.svg">
+      </h1>
+      <p>Aiolimpcss-skill<strong class="vue">Vue</strong> project</p>
+    </div>
+    <div class="content figure">
+      <figure1></figure1>
+      <figure2></figure2>
+      <figure3></figure3>
+    </div>
+  </div>
+
 </template>
 
 <style lang="scss">
-.index-page {
+::-webkit-scrollbar {
+	width: 6px;
+	height: 6px;
+}
+::-webkit-scrollbar-track {
+	background-color: rgba(#ccc, .3);
+}
+::-webkit-scrollbar-thumb {
+	border-radius: 3px;
+	background-color: #ccc;
+}
+.header {
 	overflow: hidden;
 	height: 100vh;
 	background-color: #282c34;
 	h1 {
 		margin: 20px 0;
 		font-size: 10vmin;
-	}
-	span {
-		animation-duration: 10s !important;
 	}
 	img {
 		display: inline-block;
@@ -29,8 +44,11 @@
 	}
 	p {
 		line-height: 1.5;
-		font-size: 20px;
+		font-size: 1.8vmin;
 		color: #fff;
+		& + p {
+			margin-top: 10px;
+		}
 	}
 	strong {
 		display: inline-block;
@@ -38,7 +56,6 @@
 		padding: 0 6px;
 		border-radius: 5px;
 		line-height: 20px;
-		font-size: 18px;
 		color: #fff;
 		&.default {
 			background-color: #f66;
@@ -50,12 +67,93 @@
 			background-color: #42b983;
 		}
 	}
+	div {
+		display: flex;
+		margin-top: 20px;
+	}
 	.gradient {
 		background-image: linear-gradient(92deg, #f66 0%, #f90 100%);
 		background-clip: text;
 		color: #f66;
-		animation: hue 60s infinite linear;
+		animation: hue 5s infinite linear;
+
 		-webkit-text-fill-color: transparent;
+	}
+}
+.bruce {
+	overflow: hidden;
+	position: relative;
+	padding: 50px 0;
+	border-bottom: 1px solid #ccc;
+	min-height: 300px;
+	&::before {
+		position: absolute;
+		left: 50%;
+		top: 0;
+		z-index: 99999;
+		padding: 0 10px;
+		border-radius: 0 0 5px 5px;
+		max-width: 300px;
+		line-height: 20px;
+		color: #fff;
+		content: attr(data-title);
+		transform: translateX(-50%);
+	}
+	&::after {
+		position: absolute;
+		right: 0;
+		bottom: 0;
+		z-index: 99999;
+		padding: 5px 10px;
+		border-top-left-radius: 5px;
+		background-color: #ccc;
+		font-weight: bold;
+		font-size: 16px;
+		color: #fff;
+	}
+}
+.content {
+	overflow: auto;
+	transition: all 300ms;
+	&.layout .bruce {
+		&::before {
+			background-color: #f66;
+		}
+		&::after {
+			content: "布局";
+		}
+	}
+	&.behavior .bruce {
+		&::before {
+			background-color: #66f;
+		}
+		&::after {
+			content: "行为";
+		}
+	}
+	&.color .bruce {
+		&::before {
+			background-color: #f90;
+		}
+		&::after {
+			content: "颜色";
+		}
+	}
+	&.figure .bruce {
+		&::before {
+			background-color: #09f;
+		}
+		&::after {
+			content: "图形";
+		}
+	}
+	&.component .bruce {
+		&::before {
+			background-color: #3c9;
+		}
+		&::after {
+			content: "组件";
+		}
 	}
 }
 @keyframes hue {
@@ -96,5 +194,26 @@
 </style>
 
 <script>
-export default {};
+import Figure1 from "./components/figure/树形结构和三角标";
+import Figure2 from "./components/figure/使用box-shadow裁剪聚焦区域";
+import Figure3 from "./components/figure/各种图形";
+
+export default {
+  name: "app",
+  components: {
+    //图形
+    Figure1,
+    Figure2,
+	Figure3
+  },
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  mounted() {
+    const items = document.getElementsByClassName("bruce") || [];
+    this.count = items.length;
+  },
+};
 </script>
---
+++ src/components/figure/各种图形.vue
@@ -0,0 +1,72 @@
+<template>
+  <div
+    class="bruce flex-ct-x"
+    data-title="各种图形"
+  >
+    <div class="icon star"></div>
+    <div class="icon arrow"></div>
+    <div class="icon pacman"></div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.icon {
+	display: inline-block;
+	position: relative;
+	margin-right: 20px;
+	width: 0;
+	font-size: 0;
+}
+
+.star {
+	border-left: 50px solid transparent;
+	border-right: 50px solid transparent;
+	border-bottom: 100px solid red;
+}
+
+.star::after {
+	position: absolute;
+	left: -50px;
+	top: 30px;
+	border-left: 50px solid transparent;
+	border-right: 50px solid transparent;
+	border-top: 100px solid red;
+	content: "";
+}
+
+.arrow {
+	margin: 0 40px;
+	width: 40px;
+	height: 40px;
+	background-color: red;
+}
+
+.arrow::before {
+	position: absolute;
+	left: -40px;
+	border: solid 20px red;
+	border-left-color: transparent;
+	content: "";
+}
+
+.arrow::after {
+	position: absolute;
+	right: -40px;
+	border: solid 20px transparent;
+	border-left-color: red;
+	content: "";
+}
+
+.pacman {
+	border-left: 40px solid red;
+	border-right: 40px solid transparent;
+	border-top: 40px solid red;
+	border-bottom: 40px solid red;
+	border-top-left-radius: 40px;
+	border-top-right-radius: 40px;
+	border-bottom-left-radius: 40px;
+	border-bottom-right-radius: 40px;
+	width: 0;
+	height: 0;
+}
+</style>
